732d7fcbee70b5a4eba25576db4b9c17b3eac842
divyanshu <divyanshu.r46956@gmail.com>
2017-03-27 03:15:00 +0530

    project completed !

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.static('public', { maxAge: 4 * 60 * 60 * 1000}));
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
